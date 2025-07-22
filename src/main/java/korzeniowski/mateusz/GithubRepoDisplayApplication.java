f45907f882d3e05d429566a01d64e1e7b5b71272
Mateusz Korzeniowski <m4teusz.korzeniowski@gmail.com>
2025-07-22 20:09:34 +0200

    initial commit
    Signed-off-by: Mateusz Korzeniowski <m4teusz.korzeniowski@gmail.com>

---
+++ src/main/java/korzeniowski/mateusz/GithubRepoDisplayApplication.java
@@ -0,0 +1,13 @@
+package korzeniowski.mateusz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubRepoDisplayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubRepoDisplayApplication.class, args);
+    }
+
+}
