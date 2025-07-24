6f3fcd2b3ce61dd2ae0d12be9669bc949d18a599
Mateusz Korzeniowski <m4teusz.korzeniowski@gmail.com>
2025-07-24 20:06:42 +0200

    test: add integration test checking happy path
    Signed-off-by: Mateusz Korzeniowski <m4teusz.korzeniowski@gmail.com>

--- src/test/java/korzeniowski/mateusz/controller/GitHubRepositoryControllerTest.java
+++ src/test/java/korzeniowski/mateusz/controller/GitHubRepositoryControllerTest.java
@@ -1,5 +1,37 @@
 package korzeniowski.mateusz.controller;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.everyItem;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class GitHubRepositoryControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void getRepos_ShouldReturnUserRepositoriesAndShouldReturn200() throws Exception {
+        // given
+        String username = "m4teusz-korzeniowski";
+
+        // when
+        this.mockMvc.perform(get("/" + username + "/repos"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$[0].name").isNotEmpty())
+                .andExpect(jsonPath("$[*].ownerName", everyItem(is(username))))
+                .andExpect(jsonPath("$[0].branches").isArray())
+                .andExpect(jsonPath("$[0].branches[0].name").isNotEmpty())
+                .andExpect(jsonPath("$[0].branches[0].lastCommitSha").isNotEmpty());
+    }
 }
